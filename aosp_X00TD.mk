acfbf2e354591b7b93b677a8c0956d69981d2f39
Vivekachooz <vivekachooz@gmail.com>
2020-09-03 21:00:59 +0530

    X00TD: Bringup EvolutionX
    Signed-off-by: Vivekachooz <vivekachooz@gmail.com>
    Change-Id: I000fd6558238421c0335656327b4b49361da307b

--- aosp_X00TD.mk
+++ aosp_X00TD.mk
@@ -9,18 +9,23 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common LineageOS stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from X00TD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+#KeepEvolving
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+CUSTOM_BUILD_TYPE := OFFICIAL
+
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_X00TD
+PRODUCT_NAME := aosp_X00TD
 PRODUCT_DEVICE := X00TD
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := Zenfone Max Pro M1
-PRODUCT_MANUFACTURER := asus
+PRODUCT_MANUFACTURER := Asus
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
