01c559ab2e24acb7e47b044ad91b418784e05935
fridaybabe <muhammedirfan457@gmail.com>
2021-01-20 21:43:53 +0530

    X00TD: Add avb lines to get vbmeta.img

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,3 +24,13 @@ TARGET_KERNEL_CONFIG := X00TD_defconfig
 
 # Inherit the proprietary files
 -include vendor/asus/X00TD/BoardConfigVendor.mk
+#my test kerenl comile using clang
+#TARGET_KERNEL_CLANG_COMPILE := true
+#avb code by noob
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 0
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
